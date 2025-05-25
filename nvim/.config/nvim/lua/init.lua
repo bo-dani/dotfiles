448b0271975a9c701326a68465fcb26f4fd78ec6
Daniel Mancebo <dama@bang-olufsen.dk>
2025-05-25 12:29:18 +0200

    fix clangd

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -17,6 +17,16 @@ vim.keymap.set("n", "<esc><esc>", ":noh<CR><esc>", { noremap = true, silent = tr
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+vim.g.rustfmt_autosave = 1
+
+-- Trim trailing whitespaces
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end
+})
+
 -- Plugins
 local Plug = vim.fn['plug#']
 
@@ -27,12 +37,29 @@ Plug 'nvim-lua/plenary.nvim'
 Plug 'lewis6991/gitsigns.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'folke/tokyonight.nvim'
+Plug 'catppuccin/nvim'
+Plug 'navarasu/onedark.nvim'
 Plug 'nvim-tree/nvim-web-devicons'
 Plug 'nvim-treesitter/nvim-treesitter'
 Plug('nvim-telescope/telescope-fzf-native.nvim', { ['do'] = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' })
 Plug 'nvim-lualine/lualine.nvim'
 Plug 'lukas-reineke/indent-blankline.nvim'
 Plug 'neovim/nvim-lspconfig'
+Plug 'williamboman/mason.nvim'
+Plug 'williamboman/mason-lspconfig.nvim'
+Plug 'simrat39/rust-tools.nvim'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-nvim-lsp-signature-help'
+Plug 'hrsh7th/cmp-nvim-lua'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/vim-vsnip'
+Plug 'windwp/nvim-autopairs'
+Plug 'RRethy/vim-illuminate'
+Plug 'numToStr/Comment.nvim'
+Plug 'danilamihailov/beacon.nvim'
 vim.call('plug#end')
 
 require('telescope').load_extension('fzf')
@@ -40,8 +67,11 @@ require('lualine-config')
 require('lsp-config')
 require('ibl').setup()
 require('gitsigns').setup()
+require("nvim-autopairs").setup {}
+require('Comment').setup()
+require('beacon').setup()
 
-vim.cmd('silent! colorscheme tokyonight')
+vim.cmd('silent! colorscheme onedark')
 
 -- Key bindings
 -- Telescope
@@ -51,3 +81,29 @@ vim.keymap.set('n', '<leader>f', builtin.live_grep, { silent = true, desc = "Sea
 vim.keymap.set('n', '<leader>ff', builtin.grep_string, { silent = true, desc = "Search for the string under the cursor in working directory" })
 vim.keymap.set('n', '<leader>b', builtin.buffers, { silent = true, })
 vim.keymap.set('n', '<leader>h', builtin.help_tags, { silent = true })
+
+local telescope = require('telescope')
+telescope.setup({
+  pickers = {
+    find_files = {
+      hidden = true,
+      find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
+    }
+  }
+})
+
+-- Treesitter Plugin Setup 
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "lua", "rust", "toml" },
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting=false,
+  },
+  ident = { enable = true }, 
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  }
+}
--- nvim/.config/nvim/lua/lsp-config.lua
+++ nvim/.config/nvim/lua/lsp-config.lua
@@ -6,6 +6,9 @@ local on_attach = function(client, bufnr)
 
     buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
+    vim.g.inlay_hints_visible = true
+    vim.g.diagnostics_visible = true
+
     -- Mappings
     local opts = { noremap=true, silent=true }
     buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
@@ -30,6 +33,16 @@ local on_attach = function(client, bufnr)
     elseif client.server_capabilities.documentRangeFormattingProvider then
         buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
     end
+
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+      buffer = bufnr,
+      callback = function()
+      vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+  })
+  end
 end
 
 require("lspconfig").clangd.setup({
@@ -38,13 +51,145 @@ require("lspconfig").clangd.setup({
         "clangd",
         "--pretty",
         "--header-insertion=iwyu",
-        -- "--background-index",
-        -- "--suggest-missing-includes",
-        "--query-driver=/usr/gcc-13.3/bin/arm-none-eabi-gcc",
+        "--background-index",
+        "--inlay-hints=true",
+        "--suggest-missing-includes",
+        "--query-driver=/usr/bin/arm-none-eabi-gcc",
         "-j=8",
         "--pch-storage=memory",
         "--clang-tidy",
-        "--compile-commands-dir=.",
+        "--compile-commands-dir=./.build",
     },
-    filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    filetypes = { "c", "cpp", "proto" },
+})
+
+-- Mason Setup
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "",
+            package_pending = "",
+            package_uninstalled = "",
+        },
+    }
+})
+require("mason-lspconfig").setup()
+
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
+-- LSP Diagnostics Options Setup 
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = ''})
+sign({name = 'DiagnosticSignWarn', text = ''})
+sign({name = 'DiagnosticSignHint', text = ''})
+sign({name = 'DiagnosticSignInfo', text = ''})
+
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    update_in_insert = true,
+    underline = true,
+    severity_sort = false,
+    float = {
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
+})
+
+vim.cmd([[
+  set signcolumn=yes
+  autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+--Set completeopt to have a better completion experience
+-- :help completeopt
+-- menuone: popup even when there's only one match
+-- noinsert: Do not insert text until a selection is made
+-- noselect: Do not select, force to select one from the menu
+-- shortness: avoid showing extra messages when using completion
+-- updatetime: set updatetime for CursorHold
+vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
+vim.opt.shortmess = vim.opt.shortmess + { c = true}
+vim.api.nvim_set_option('updatetime', 300) 
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error 
+-- Show inlay_hints more frequently 
+vim.cmd([[
+  set signcolumn=yes
+  autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
+
+-- Completion Plugin Setup
+local cmp = require'cmp'
+cmp.setup({
+  -- Enable LSP snippets
+  snippet = {
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    -- Add tab support
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    })
+  },
+  -- Installed sources:
+  sources = {
+    { name = 'path' },                              -- file paths
+    { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
+    { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
+    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
+    { name = 'buffer', keyword_length = 2 },        -- source current buffer
+    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
+    { name = 'calc'},                               -- source for math calculation
+  },
+  window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+      fields = {'menu', 'abbr', 'kind'},
+      format = function(entry, item)
+          local menu_icon ={
+              nvim_lsp = 'λ',
+              vsnip = '⋗',
+              buffer = 'Ω',
+              path = '🖫',
+          }
+          item.menu = menu_icon[entry.source.name]
+          return item
+      end,
+  },
 })
